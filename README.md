681eb6fce0b308de107ebe80e25b0d640b668205
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2023-08-06 02:09:59 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Photo Challenge
+
 Service for daily photo challenges
+
+## Pre-requisites
+
+- [Node 16+](https://nodejs.org/en)
+- [Bazel](https://bazel.build/install/bazelisk)
+  - MacOS: `brew install bazelisk`
+  - Windows: `choco install bazelisk` (Needs [chocolatey](https://chocolatey.org/install))
+
+## Building
+
+- MacOS `./script.zsh build`
+- Windows `script.bat build`
+
+## Running
+
+- MacOS `./script.zsh run`
+- Windows `script.bat run`
--- script.bat
+++ script.bat
@@ -12,10 +12,10 @@ SET "SCRIPT_NAME=%~1"
 SET "SCRIPT_NAME=%SCRIPT_NAME::=_%"
 
 REM Check if the script file exists in the scripts directory
-IF NOT EXIST "scripts\%SCRIPT_NAME%.zsh" (
-  echo Error: Script file 'scripts\%SCRIPT_NAME%.zsh' not found.
+IF NOT EXIST "scripts\%SCRIPT_NAME%.bat" (
+  echo Error: Script file 'scripts\%SCRIPT_NAME%.bat' not found.
   exit /b 1
 )
 
 REM Execute the second script
-CALL scripts\%SCRIPT_NAME%.zsh
+CALL scripts\%SCRIPT_NAME%.bat
